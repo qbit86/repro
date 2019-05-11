118e938783b0cbb576871bdd6e6dde24a8c5f362
Viktor T <qbit86@gmail.com>
2019-05-12 00:47:55 +0300

    Add Empty struct

--- src/ExpressionBody/Program.cs
+++ src/ExpressionBody/Program.cs
@@ -1,5 +1,26 @@
+using System;
+
 namespace Repro
 {
+    internal readonly struct Empty : IEquatable<Empty>
+    {
+        public bool Equals(Empty other) => true;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Empty other && Equals(other);
+        }
+
+        public override int GetHashCode() => 1729;
+
+        public static bool operator ==(Empty left, Empty right) => left.Equals(right);
+
+        public static bool operator !=(Empty left, Empty right)
+        {
+            return !left.Equals(right);
+        }
+    }
+
     internal static class Program
     {
         private static void Main()
