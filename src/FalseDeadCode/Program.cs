a41c2a6b60ec818501ba4e62ba378e598b378720
Viktor T <qbit86@gmail.com>
2019-06-10 02:28:59 +0300

    Add repro

--- src/FalseDeadCode/Program.cs
+++ src/FalseDeadCode/Program.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
 namespace Repro
 {
     internal static class Program
     {
         private static void Main()
         {
+            List<Exception> exceptions = null;
+            for (int i = 0; i != 1; ++i)
+            {
+                try
+                {
+                    ThrowInvalidOperationException(i.ToString(CultureInfo.InvariantCulture));
+                }
+#pragma warning disable CA1031 // Do not catch general exception types
+                catch (Exception ex)
+                {
+                    if (exceptions is null)
+                        exceptions = new List<Exception>();
+
+                    exceptions.Add(ex);
+                }
+#pragma warning restore CA1031 // Do not catch general exception types
+            }
+
+            Console.WriteLine($"{nameof(exceptions.Count)}: {exceptions?.Count}");
+        }
+
+        private static void ThrowInvalidOperationException(string message)
+        {
+            throw new InvalidOperationException(message);
         }
     }
 }
