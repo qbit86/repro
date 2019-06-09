c9c3e976f37e96f360161743e62fc89882ba4637
Viktor T <qbit86@gmail.com>
2019-06-10 02:38:25 +0300

    Fix repro

--- src/FalseDeadCode/Program.cs
+++ src/FalseDeadCode/Program.cs
@@ -9,7 +9,7 @@ namespace Repro
         private static void Main()
         {
             List<Exception> exceptions = null;
-            for (int i = 0; i != 1; ++i)
+            for (int i = 0; i != 2; ++i)
             {
                 try
                 {
