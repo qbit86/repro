e7c0b9af879cea32030777cb7f19c0043c2ae2dc
Viktor T <qbit86@gmail.com>
2019-12-11 22:50:18 +0300

    Add example of warning

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable UnusedVariable
+
+namespace Repro
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // Expected: green squiggle on `var`, light bulb “Roslyn Code Fixes” with “Use explicit type” command.
+            // Observed: as expected.
+            var xs = new[] { "Uno", "Dos", "Tres" };
+        }
+    }
+}
