c78e3ae313d7aabeef9413e09fe347b2b8b72cea
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-03 01:55:53 +0300

    Add printing interleaving lines

---
+++ RiderRunWindowRepro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Repro
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            for (int i = 0; i != 32; ++i)
+            {
+                string linePrefix = (i & 1) == 0 ? "██ " : "░░ ";
+                Console.WriteLine(linePrefix + i.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
