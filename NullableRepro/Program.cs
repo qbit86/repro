ab9f9179d9685f91be8b35811e21243a2327f5cb
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-04 23:31:44 +0300

    Add NullableRepro

---
+++ NullableRepro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Repro
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Foo("Hello World!");
+            Foo(null);
+        }
+
+        private static void Foo(string? s)
+        {
+            Console.WriteLine($"“{s}”");
+        }
+    }
+}
