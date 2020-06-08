31f43e2abf8c4ba4babf13149f1c8bff33a87a2b
Viktor Tselov <lightmap.tselov@gmail.com>
2020-06-08 16:55:28 +0300

    Create Monoid benchmark

---
+++ MonoidDemo/MyBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace Repro
+{
+    public abstract class MyBenchmark
+    {
+        private static readonly int[] s_integers = { 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+        private static readonly decimal[] s_decimals = { 2m, 3m, 5m, 8m, 13m, 21m, 34m, 55m, 89m };
+
+        [Benchmark(Baseline = true)]
+        public int ReduceIntegers() => s_integers.Reduce(default(AdditiveInt32Monoid));
+
+        [Benchmark]
+        public int AggregateIntegers() => s_integers.Aggregate(0, (left, right) => left + right);
+
+        [Benchmark]
+        public decimal ReduceDecimals() => s_decimals.Reduce(default(MultiplicativeDecimalMonoid));
+
+        [Benchmark]
+        public decimal AggregateDecimals() => s_decimals.Aggregate(1m, (left, right) => left * right);
+    }
+}
---
+++ MonoidDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Repro
+{
+    internal static class Program
+    {
+        internal static T Reduce<T, TMonoid>(this IEnumerable<T> items, TMonoid monoid)
+            where TMonoid : IMonoid<T>
+        {
+            if (items is null)
+                throw new ArgumentNullException(nameof(items));
+
+            T result = monoid.Identity;
+            foreach (T item in items)
+                result = monoid.Combine(result, item);
+
+            return result;
+        }
+
+        private static void Main()
+        {
+            Job job = new Job(Job.Default).ApplyAndFreeze(RunMode.Short);
+            IConfig config = ManualConfig.Create(DefaultConfig.Instance).AddJob(job);
+            Summary _ = BenchmarkRunner.Run<MyBenchmark>(config);
+        }
+    }
+}
