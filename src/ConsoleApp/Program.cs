37c464c0426c795ec95d6241b2a4dca884d356a6
Viktor T <qbit86@gmail.com>
2021-10-24 10:54:19 +0300

    Add repro for cleanup non-idempotence

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+[assembly: CLSCompliant(true)]
+
+namespace Repro
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            if (true)
+                await Console.Out.WriteLineAsync("Lorem ipsum dolor sit amet, consectetur adipiscing elit").ConfigureAwait(false);
+        }
+    }
+}
