0d6566c1483c9c4e92c47dbe57f9ff8debbf514b
Viktor Ptitselov <qbit86@gmail.com>
2023-02-14 12:02:59 +0200

    Add project OddEmptyLine

---
+++ src/OddEmptyLine/OddEmptyLine.cs
@@ -0,0 +1,9 @@
+﻿namespace Repro;
+
+#if NETSTANDARD2_1_OR_GREATER
+using MyEnumerator = System.ArraySegment<int>.Enumerator;
+#else
+using MyEnumerator = System.Collections.Generic.IEnumerator<int>;
+#endif
+
+public static class OddEmptyLine { }
