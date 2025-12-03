35c32a2b1e9ec21dede4c81d37950653bf400244
Viktor Tselov <viktor.tselov@jetbrains.com>
2025-12-04 00:55:02 +0200

    Add UnityAgnostic and UnityAware modules with initial scripts and metadata

---
+++ MyGame-Unity/Assets/MyGame/Scripts/MyGame.UnityAgnostic/UnityAgnosticExample.cs
@@ -0,0 +1,8 @@
+using System.Numerics;
+
+namespace MyGame;
+
+public static class UnityAgnosticExample
+{
+    public static Vector3 CrossProduct(Vector3 a, Vector3 b) => Vector3.Cross(a, b);
+}
---
+++ MyGame-Unity/Assets/MyGame/Scripts/MyGame.UnityAware/UnityAwareExample.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace MyGame;
+
+public static class UnityAwareExample
+{
+    public static Vector3 CrossProduct(Vector3 a, Vector3 b) => Vector3.Cross(a, b);
+}
