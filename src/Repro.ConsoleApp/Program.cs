884eea08420c98220298936eef9851b5b9a5a8eb
Viktor Tselov <viktor.tselov@jetbrains.com>
2026-05-04 14:00:21 +0300

    Implement debugger attachment logic in Main method

--- src/Repro.ConsoleApp/Program.cs
+++ src/Repro.ConsoleApp/Program.cs
@@ -1,8 +1,31 @@
-﻿namespace Repro;
+﻿using System;
+using System.Diagnostics;
+
+namespace Repro;
 
 internal static class Program
 {
     private static void Main()
     {
+        if (!Debugger.IsAttached)
+        {
+            Console.WriteLine($"Waiting for debugger to attach. ProcessId: {Environment.ProcessId}");
+#if REPRO_ATTACH_MANUALLY
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+            } while (!Debugger.IsAttached);
+#else
+            Debugger.Launch();
+#endif
+        }
+
+        Console.WriteLine($"IsAttached: {Debugger.IsAttached}");
+        Debugger.Break();
+        if (!Console.IsInputRedirected)
+        {
+            Console.WriteLine("Press any key to continue...");
+            _ = Console.ReadKey();
+        }
     }
 }
